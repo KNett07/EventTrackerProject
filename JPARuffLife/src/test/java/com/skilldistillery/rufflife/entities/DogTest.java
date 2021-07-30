4c56db2bfc9d6213a90ff2536d9b90f7835eb9a2
Kim Nettleton <kim@Brucebs-MacBook-Pro.local>
2021-07-30 16:09:42 -0600

    update to JPA And Boot

---
+++ JPARuffLife/src/test/java/com/skilldistillery/rufflife/entities/DogTest.java
@@ -0,0 +1,50 @@
+package com.skilldistillery.rufflife.entities;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class DogTest {
+	
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	private Dog dog;
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("JPARuffLife");
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+		emf.close();
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+		em = emf.createEntityManager();
+		dog = em.find(Dog.class, 1);
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		em.close();
+		dog = null;
+	}
+
+	@Test
+	void test_Dog_Entity_Mapping() {
+		assertNotNull(dog);
+		assertEquals("Tuco", dog.getName());
+	}
+
+}
---
+++ RuffLife/src/main/java/com/skilldistillery/rufflife/RuffLifeApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.rufflife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuffLifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RuffLifeApplication.class, args);
+	}
+
+}
